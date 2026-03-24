290676cce50d550bd022b0fce3de87d5c4bd77f1
Max Spiridonov <taijased@gmail.com>
2026-03-24 18:39:56 +0300

    some fixes

--- apps/miniapp/src/features/profile/services/profile-service.ts
+++ apps/miniapp/src/features/profile/services/profile-service.ts
@@ -3,9 +3,9 @@ import { apiRequest } from "../../../lib/api";
 
 export const getEmptyProfileSummary = (): ProfileSummary => ({
   displayName: "Mini App User",
-  username: "",
+  username: "Test User",
   telegramId: "miniapp-local-user",
-  avatarUrl: null,
+  avatarUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGCpcS07MvEGYFPZINYLTps_SH9QB69rjkzLmezGGPpyl2ENWs8yv-IZTOgcMj4TdAibjjUVwRkv3lTQo51i9Q-R9o09030UBZmSNJQUk&s=10",
   isTelegramVerified: false,
 });
 
